92a0dff66266af33755e2d236d6dab020fe341b4
ffalor <35144141+ffalor@users.noreply.github.com>
2022-10-25 21:44:00 +0000

    wip

--- Policyfile.rb
+++ Policyfile.rb
@@ -9,8 +9,5 @@ name 'chef-falcon'
 # Where to find external cookbooks:
 default_source :supermarket
 
-# run_list: chef-client will run these recipes in the order specified.
-run_list 'chef-falcon::default'
-
 # Specify a custom source for a single cookbook:
 cookbook 'chef-falcon', path: '.'
