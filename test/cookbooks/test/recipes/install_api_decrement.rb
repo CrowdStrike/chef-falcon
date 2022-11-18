6db835e8c0598491b7ff7861f5f0b2d8e453d924
Carlos Matos <carlos.matos@crowdstrike.com>
2022-11-18 09:05:08 -0500

    Add new way to test library functionality for decrement

--- test/cookbooks/test/recipes/install_api_decrement.rb
+++ test/cookbooks/test/recipes/install_api_decrement.rb
@@ -1,9 +1,27 @@
+client_id = ENV['FALCON_CLIENT_ID']
+client_secret = ENV['FALCON_CLIENT_SECRET']
+falcon_cloud = ENV['FALCON_CLOUD']
+sensor_tmp_dir = '/tmp'
+
 falcon_install 'falcon' do
-  client_id ENV['FALCON_CLIENT_ID']
-  client_secret ENV['FALCON_CLIENT_SECRET']
-  falcon_cloud ENV['FALCON_CLOUD']
+  client_id client_id
+  client_secret client_secret
+  falcon_cloud falcon_cloud
   version_decrement 2
   action :install
+end
+
+# Use the helpers library to get the version of the requested package
+::Chef::DSL::Recipe.send(:include, ChefFalcon::Helpers)
+
+sensor_info = sensor_download_info(client_id, client_secret, {
+    version_decrement: 2,
+    sensor_tmp_dir: sensor_tmp_dir,
+    falcon_cloud: falcon_cloud,
+  })
+
+file "#{sensor_tmp_dir}/version" do
+  content sensor_info['version']
 end
 
 include_recipe 'test::common'
--- test/integration/install_api_decrement/controls/default.rb
+++ test/integration/install_api_decrement/controls/default.rb
@@ -1,4 +1,11 @@
 # Include common controls
 include_controls 'common'
 
-# TODO: Figure out how to get sensor version from API
+# We want to ensure the package version installed matches the version we expect
+# from the /tmp/version file
+sensor_tmp_dir = '/tmp'
+sensor_version_file = File.join(sensor_tmp_dir, 'version')
+
+describe package('falcon-sensor') do
+  its('version') { should match file(sensor_version_file).content.strip }
+end
