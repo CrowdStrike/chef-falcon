9a77709f0e9a43f8e8e575dbb49e54d81df1901c
Carlos Matos <matosc15@gmail.com>
2023-06-09 00:05:35 -0400

    We only need to ensure package is installed. (#50)
    No need to check for a specific version. If this succeeds, then
    we know the policy worked.

--- test/integration/install_api_policy/controls/default.rb
+++ test/integration/install_api_policy/controls/default.rb
@@ -1,9 +1,2 @@
-falcon_version = '6.48.0-14504' # This is from Sensor Update Policy
-
-# Include common controls
+# Include common::installed tests
 include_controls 'common'
-
-# Ensure falcon-sensor package installed to specific version
-describe package('falcon-sensor') do
-  its('version') { should match falcon_version }
-end
